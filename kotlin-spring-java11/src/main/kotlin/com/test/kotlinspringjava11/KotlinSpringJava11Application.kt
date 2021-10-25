91c2d769fbec074fd8fd851000157475c563bb8c
chess.min <chess.min@kakaocorp.com>
2021-10-25 10:12:41 +0900

    project init

---
+++ kotlin-spring-java11/src/main/kotlin/com/test/kotlinspringjava11/KotlinSpringJava11Application.kt
@@ -0,0 +1,11 @@
+package com.test.kotlinspringjava11
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringJava11Application
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringJava11Application>(*args)
+}
---
+++ kotlin-spring-java11/src/test/kotlin/com/test/kotlinspringjava11/KotlinSpringJava11ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.test.kotlinspringjava11
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringJava11ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
