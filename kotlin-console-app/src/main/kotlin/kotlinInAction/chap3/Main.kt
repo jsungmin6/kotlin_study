60e332368d82056b30030d4c835d55d195a5a1f8
jsungmin6 <jsungmin506@gmail.com>
2021-12-01 10:47:32 +0900

    kotlin in action chater3

--- kotlin-console-app/src/main/kotlin/kotlinInAction/chap3/Main.kt
+++ kotlin-console-app/src/main/kotlin/kotlinInAction/chap3/Main.kt
@@ -1,11 +1,14 @@
 package kotlinInAction.chap3
 
+
+
 //초안
 fun <T> joinToStirng1(
     collection: Collection<T>,
     separator: String,
     prefix: String,
     postfix: String
+
 ):String {
     val result = StringBuilder(prefix)
     for ((index, element) in collection.withIndex()) {
@@ -32,6 +35,27 @@ fun <T> joinToString2(
     return result.toString()
 }
 
+//확장함수 사용
+fun <T> Collection<T>.joinToString3(
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = ""
+):String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in this.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+//확장 프로퍼티
+val String.lastChar1: Char get() = get(length - 1)
+var StringBuilder.lastChar2: Char
+    get() =get(length - 1)
+    set(value: Char) { this.setCharAt(length - 1, value) }
+
 fun main(args: Array<String>) {
     //set
     val hashSetOf = hashSetOf(1, 6)
@@ -47,4 +71,30 @@ fun main(args: Array<String>) {
     //디폴트 사용
     joinToString2(list)
     joinToString2(list,postfix = "#")
+
+    //확장함수 사용
+    val el = arrayListOf(1, 2, 3)
+    println(el.joinToString3(" "))
+
+
+
+    //확장함수는 오버라이드할 수 없다
+    open class View {
+        open fun click() = println("View Click!")
+    }
+    class Button: View() {
+        override fun click() = println("Button Click!")
+    }
+    fun View.showOff() = println("view")
+    fun Button.showoff() = println("botton")
+    val view: View = Button() //
+    view.showOff() // <= view
+
+    //확장 프로퍼티
+    println("Kotlin".lastChar1)
+    val sb = StringBuilder("Kotlin?")
+    sb.lastChar2 = '!'
+    println(sb)
+
+
 }
